ad528581a89f79a58a54980f2dcd9471e915142a
Nicolas <nicolas.baechler@outlook.com>
2025-05-15 16:16:31 +0200

    src/Controllers/AdminController.php - Déclare SiteController au lieu de AdminController src/Controllers/AscentController.php - Déclare aussi SiteController au lieu de AscentController

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -1,12 +1,12 @@
 <?php
-// src/Controllers/SiteController.php
+// src/Controllers/AdminController.php
 namespace TopoclimbCH\Controllers;
 
 use TopoclimbCH\Core\View;
 use TopoclimbCH\Core\Session;
 use TopoclimbCH\Core\Response;
 
-class SiteController extends BaseController
+class AdminController extends BaseController
 {
     public function __construct(View $view, Session $session)
     {
@@ -15,6 +15,16 @@ class SiteController extends BaseController
     
     public function index(): Response
     {
-        return $this->redirect('/');
+        return $this->render('admin/index.twig', [
+            'title' => 'Administration'
+        ]);
+    }
+    
+    public function users(): Response
+    {
+        // Logique pour afficher la liste des utilisateurs
+        return $this->render('admin/users.twig', [
+            'title' => 'Gestion des utilisateurs'
+        ]);
     }
 }
--- src/Controllers/AscentController.php
+++ src/Controllers/AscentController.php
@@ -1,20 +1,35 @@
 <?php
-// src/Controllers/SiteController.php
+// src/Controllers/AscentController.php
 namespace TopoclimbCH\Controllers;
 
 use TopoclimbCH\Core\View;
 use TopoclimbCH\Core\Session;
 use TopoclimbCH\Core\Response;
+use TopoclimbCH\Core\Auth;
 
-class SiteController extends BaseController
+class AscentController extends BaseController
 {
-    public function __construct(View $view, Session $session)
+    // Changez private en protected (ou public) selon la définition dans BaseController
+    protected $auth;
+    
+    public function __construct(View $view, Session $session, Auth $auth)
     {
         parent::__construct($view, $session);
+        $this->auth = $auth;
     }
     
-    public function index(): Response
+    public function create(int $route_id): Response
     {
-        return $this->redirect('/');
+        return $this->render('ascents/create.twig', [
+            'title' => 'Ajouter une ascension',
+            'route_id' => $route_id
+        ]);
+    }
+    
+    public function store(): Response
+    {
+        // Logique pour enregistrer une nouvelle ascension
+        $this->session->flash('success', 'Ascension enregistrée avec succès');
+        return $this->redirect('/mes-voies');
     }
 }
